8425665c5b8bc72cd86da002521f0eb5ee9ac3a5
JulianPDT <julian.rozewicz@gmail.com>
2022-04-08 23:04:03 +0200

    poprawione hiperlinki

--- freeGanNET.py
+++ freeGanNET.py
@@ -5,7 +5,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
+    return render_template("home.html")
+
+@app.route('/giver')
+def giver():
     return render_template("giver.html")
+
+@app.route('/taker')
+def taker():
+    return render_template("takerhtml")
 
 
 if __name__ == "__main__":
